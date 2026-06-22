c231e8e608390a0be3ac362cb9853a5eff57e4dc
Zain kiki <zainkiki888@gmail.com>
2026-06-22 11:23:37 +0200

    something is working fr fr

--- LeaderboardLabel.cs
+++ LeaderboardLabel.cs
@@ -1,15 +1,58 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 public partial class LeaderboardLabel : Label
 {
-	// Called when the node enters the scene tree for the first time.
+	private HttpRequest _httpRequest;
 	public override void _Ready()
 	{
+		_httpRequest = new HttpRequest();
+		AddChild(_httpRequest);
+
+        _httpRequest.RequestCompleted += OnRequestCompleted;
+		var error = _httpRequest.Request("http://localhost:5049/leaderboard");
+
+		if (error != Error.Ok)
+		{
+			Text = $"Request faild: {error}";
+		}
+
+    }
+	private void OnRequestCompleted(
+		long result,
+		long responseCode,
+		string[] headers,
+		byte[] body)
+	{
+		string json = System.Text.Encoding.UTF8.GetString(body);
+		GD.Print(json);
+
+		try
+		{
+			List<PlayerScore> leaderboard = JsonSerializer.Deserialize<List<PlayerScore>>(json);
+
+			string displayText = "Leaderboard\n\n";
+
+			foreach (var player in leaderboard)
+			{
+				displayText += $"{player.playerName}: {player.score}\n";
+			}
+			Text = displayText;
+		}
+		catch (Exception ex)
+		{
+			Text = $"JSON Error: {ex.Message}";
+		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public class PlayerScore
+	{
+		public string playerName { get; set; }
+		public int score { get; set; }
+	}
+    public override void _Process(double delta)
 	{
 	}
 }
